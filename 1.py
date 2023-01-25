504472fd82d61594527f69621775a79f8bca2756
Novikova555Anna <123547082+Novikova555Anna@users.noreply.github.com>
2023-01-25 16:53:22 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-class Car():
+class Car:
     """
     Автомобиль
     имеет цвет
@@ -8,9 +8,9 @@ class Car():
     """
     color: str = 'red'  # цвет
     power: int = 100  # мощность двигателя
-    tank_capacity = 50  # объем бака
-    fuel_consumption = 15  # расход топлива на 100 км
-    fuel_now = 0  # топлива в баке сейчас
+    tank_capacity: int = 50  # объем бака
+    fuel_consumption: int = 15  # расход топлива на 100 км
+    fuel_now: int = 0  # топлива в баке сейчас
 
     def __init__(self,  color: str = 'red', power: int = 100, tank_capacity = 50, fuel_consumption = 15, fuel_now = 0):
         self.color = color
@@ -35,9 +35,57 @@ class Car():
         return self.power
 
     def get_fuel_consumption(self):
-        self.fuel_consumption = self.fuel_now / self.fuel_consumption
+        self.fuel_consumption = self.fuel_now/self.fuel_consumption
         return self.fuel_consumption
 
+    def print_all(self):
+        print(self.color)
+        print(self.fuel_now)
+        print(self.tank_capacity)
+        print(self.power)
+        print(self.fuel_consumption)
+
+def test_init_1():
+    Car(color='red',
+        engine_power=100,
+        tank_capacity=50,
+        fuel_consumption=15,
+        fuel_now=0)
+
+
+def test_init_2():
+    Car(color='red')
+
+
+def test_color_1():
+    BMV = Car(color='red',
+            engine_power=100,
+            tank_capacity=50,
+            fuel_consumption=15,
+            fuel_now=0)
+    assert BMV.get_color() == 'red'
+
+
+def test_color_2():
+    BMV = Car(color='red',
+            engine_power=100,
+            tank_capacity=50,
+            fuel_consumption=15,
+            fuel_now=0)
+    assert BMV.get_color() == 'red'
+    BMV.set_color('red')
+
+
+def test_color_3():
+    BMV = Car(color='red',
+            engine_power=100,
+            tank_capacity=50,
+            fuel_consumption=15,
+            fuel_now=0)
+    assert BMV.get_color() == 'red'
+    BMV.set_color('red')
+
+    assert BMV.get_color() == 'red'
 BMV = Car()
 BMV.set_color()
 BMV.get_fuel_now()
@@ -49,3 +97,5 @@ print(BMV.get_fuel_now())
 print(BMV.set_fuel_now())
 print(BMV.get_engine_power())
 print(BMV.get_fuel_consumption())
+print()
+BMV.print_all()
